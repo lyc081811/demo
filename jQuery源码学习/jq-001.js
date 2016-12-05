6eee3aa6b5164734f6852b0cbfc2d847a9f21dff
lyc081811 <269632358@qq.com>
2016-12-05 21:40:33 +0800

    blue老师jq源码的第一节

---
+++ jQuery源码学习/jq-001.js
@@ -0,0 +1,8 @@
+(function(){
+	(21~94)定义了一些变量和函数 jQuery = function(){};
+	(96~283)给jq对象，添加方法和属性；
+	(285~347)extend:jq的继承方法；
+	(349~817)jQuery.extend:扩展一些工具方法；
+	
+	(8826)-> window.jQuery = window.$ = jQuery;
+})();
